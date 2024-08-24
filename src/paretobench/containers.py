137121ba77288f78b56b6c2ed585a5dbf199fb2e
Christopher M. Pierce <chris@chris-pierce.com>
2024-08-23 20:55:06 -0700

    adding another file format to compare performance

--- src/paretobench/containers.py
+++ src/paretobench/containers.py
@@ -29,10 +29,20 @@ class Population:
             g=g['g'][()],
             feval=g.attrs['feval']
         )
+        
+    def __len__(self):
+        return self.x.shape[0]
 
 
 @dataclass
 class History:
+    """
+    Reprsents the "history" of an optimizatoin algorithm solving a multiobjective optimization problem. Populations are recorded
+    at some reporting interval.
+    
+    Assumptions:
+     - All reports must have consistent number of objectives, decision variables, and constraints.
+    """
     reports: List[Population]
     problem: str
     metadata: Dict[str, Union[str, int, float, bool]]
@@ -57,6 +67,43 @@ class History:
             if m:
                 idx_reports.append((int(m.group(1)), Population._from_h5py_group(report_grp)))
         reports = [x[1] for x in sorted(idx_reports, key=lambda x: x[0])]
+
+        # Return as a history object
+        return cls(
+            problem=g.attrs['problem'],
+            reports=reports,
+            metadata={k: v for k, v in g['metadata'].attrs.items()},
+        )
+
+    def _to_h5py_group_v2(self, g: h5py.Group):
+        # Save the metadata
+        g.attrs['problem'] = self.problem
+        g_md = g.create_group("metadata")
+        for k, v in self.metadata.items():
+            g_md.attrs[k] = v
+        
+        # Save data from each population into one bigger dataset to reduce API calls to HDF5 file reader
+        g.attrs['pop_sizes'] = [len(r) for r in self.reports]
+        g.attrs['fevals'] = [r.feval for r in self.reports]
+        g['x'] = np.concatenate([r.x for r in self.reports], axis=0)
+        g['f'] = np.concatenate([r.f for r in self.reports], axis=0)
+        g['g'] = np.concatenate([r.g for r in self.reports], axis=0)
+
+    @classmethod
+    def _from_h5py_group(cls, g: h5py.Group):
+        start_idx = 0
+        reports = []
+        x = g['x'][()]
+        f = g['f'][()]
+        g = g['g'][()]
+
+        for pop_size, feval in zip(g.attrs['pop_sizes'], g.attrs['fevals']):
+            reports.append(Population(
+                x = x[start_idx:start_idx+pop_size],
+                f = f[start_idx:start_idx+pop_size],
+                g = g[start_idx:start_idx+pop_size],
+                feval=feval,
+            ))
 
         # Return as a history object
         return cls(
