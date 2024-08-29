9929e7f37dd2f3882989b6d3e8313af02eab0b46
Christopher M. Pierce <chris@chris-pierce.com>
2024-08-28 18:56:05 -0700

    choose "version 2" save format over "version 1"

--- src/paretobench/containers.py
+++ src/paretobench/containers.py
@@ -59,24 +59,6 @@ class Population:
         g = np.random.rand(pop_size, n_constraints) if n_constraints > 0 else np.empty((pop_size, 0))
         return cls(x=x, f=f, g=g, feval=feval)
     
-    def _to_h5py_group(self, g: h5py.Group):
-        # Write datasets
-        g['x'] = self.x
-        g['f'] = self.f
-        g['g'] = self.g
-        
-        # Add metadata
-        g.attrs['feval'] = self.feval
-
-    @classmethod
-    def _from_h5py_group(cls, g: h5py.Group):
-        return cls(
-            x=g['x'][()],
-            f=g['f'][()],
-            g=g['g'][()],
-            feval=g.attrs['feval']
-        )
-        
     def __len__(self):
         return self.x.shape[0]
 
@@ -147,34 +129,6 @@ class History:
         for k, v in self.metadata.items():
             g_md.attrs[k] = v
         
-        # Save each report into its own group
-        for idx, report in enumerate(self.reports):
-            report._to_h5py_group(g.create_group("report_{:d}".format(idx)))
-
-    @classmethod
-    def _from_h5py_group(cls, g: h5py.Group):
-        # Load each of the runs keeping track of the order of the indices
-        idx_reports = []
-        for idx_str, report_grp in g.items():
-            m = re.match(r'report_(\d+)', idx_str)
-            if m:
-                idx_reports.append((int(m.group(1)), Population._from_h5py_group(report_grp)))
-        reports = [x[1] for x in sorted(idx_reports, key=lambda x: x[0])]
-
-        # Return as a history object
-        return cls(
-            problem=g.attrs['problem'],
-            reports=reports,
-            metadata={k: v for k, v in g['metadata'].attrs.items()},
-        )
-
-    def _to_h5py_group_v2(self, g: h5py.Group):
-        # Save the metadata
-        g.attrs['problem'] = self.problem
-        g_md = g.create_group("metadata")
-        for k, v in self.metadata.items():
-            g_md.attrs[k] = v
-        
         # Save data from each population into one bigger dataset to reduce API calls to HDF5 file reader
         g.attrs['pop_sizes'] = [len(r) for r in self.reports]
         g.attrs['fevals'] = [r.feval for r in self.reports]
@@ -183,7 +137,7 @@ class History:
         g['g'] = np.concatenate([r.g for r in self.reports], axis=0)
 
     @classmethod
-    def _from_h5py_group_v2(cls, grp: h5py.Group):
+    def _from_h5py_group(cls, grp: h5py.Group):
         start_idx = 0
         reports = []
         x = grp['x'][()]
@@ -262,28 +216,20 @@ class Experiment:
                     run._to_h5py_group(f.create_group("run_{:d}".format(idx)))
             elif version == 2:
                 for idx, run in enumerate(self.runs):
-                    run._to_h5py_group_v2(f.create_group("run_{:d}".format(idx)))
+                    run._to_h5py_group(f.create_group("run_{:d}".format(idx)))
             else:
                 raise ValueError(f'unrecognized version: {version}')
                 
     @classmethod
-    def load(cls, fname, version=1):        
+    def load(cls, fname):        
         # Load the data
         with h5py.File(fname, mode='r') as f:
             # Load each of the runs keeping track of the order of the indices
             idx_runs = []
-            if version == 1:
-                for idx_str, run_grp in f.items():
-                    m = re.match(r'run_(\d+)', idx_str)
-                    if m:
-                        idx_runs.append((int(m.group(1)), History._from_h5py_group(run_grp)))
-            elif version == 2:
-                for idx_str, run_grp in f.items():
-                    m = re.match(r'run_(\d+)', idx_str)
-                    if m:
-                        idx_runs.append((int(m.group(1)), History._from_h5py_group_v2(run_grp)))
-            else:
-                raise ValueError(f'unrecognized version: {version}')
+            for idx_str, run_grp in f.items():
+                m = re.match(r'run_(\d+)', idx_str)
+                if m:
+                    idx_runs.append((int(m.group(1)), History._from_h5py_group(run_grp)))
             runs = [x[1] for x in sorted(idx_runs, key=lambda x: x[0])]
             
             # Return as an experiment object
