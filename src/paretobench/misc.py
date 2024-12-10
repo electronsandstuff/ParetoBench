72fb63c021b2010943d5cf0340d64ede418bbb88
Christopher M. Pierce <chris@chris-pierce.com>
2024-12-09 22:35:37 -0800

    automatic formatting on all files

--- src/paretobench/misc.py
+++ src/paretobench/misc.py
@@ -10,6 +10,7 @@ class SCH(Problem):
 
     Other name: Schaffer function N. 1
     """
+
     @property
     def n(self):
         return 1
@@ -17,28 +18,27 @@ class SCH(Problem):
     @property
     def m(self):
         return 2
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        return Population(f=np.vstack((
-            x[0] ** 2,
-            (x[0] - 2) ** 2
-        )).T)
-        
+
+        return Population(f=np.vstack((x[0] ** 2, (x[0] - 2) ** 2)).T)
+
     @property
     def var_lower_bounds(self):
-        return -1e3*np.ones(self.n)
-    
+        return -1e3 * np.ones(self.n)
+
     @property
     def var_upper_bounds(self):
-        return 1e3*np.ones(self.n)
+        return 1e3 * np.ones(self.n)
 
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
 
 
 class FON(Problem):
@@ -47,6 +47,7 @@ class FON(Problem):
 
     Other name: Fonseca-Fleming Function
     """
+
     @property
     def n(self):
         return 3
@@ -54,28 +55,44 @@ class FON(Problem):
     @property
     def m(self):
         return 2
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        return Population(f=np.array([
-            1 - np.exp(-(x[0] - 1 / np.sqrt(3)) ** 2 - (x[1] - 1 / np.sqrt(3)) ** 2 - (x[2] - 1 / np.sqrt(3)) ** 2),
-            1 - np.exp(-(x[0] + 1 / np.sqrt(3)) ** 2 - (x[1] + 1 / np.sqrt(3)) ** 2 - (x[2] + 1 / np.sqrt(3)) ** 2),
-        ]).T)
+
+        return Population(
+            f=np.array(
+                [
+                    1
+                    - np.exp(
+                        -((x[0] - 1 / np.sqrt(3)) ** 2)
+                        - (x[1] - 1 / np.sqrt(3)) ** 2
+                        - (x[2] - 1 / np.sqrt(3)) ** 2
+                    ),
+                    1
+                    - np.exp(
+                        -((x[0] + 1 / np.sqrt(3)) ** 2)
+                        - (x[1] + 1 / np.sqrt(3)) ** 2
+                        - (x[2] + 1 / np.sqrt(3)) ** 2
+                    ),
+                ]
+            ).T
+        )
 
     @property
     def var_lower_bounds(self):
-        return -4*np.ones(self.n)
-    
+        return -4 * np.ones(self.n)
+
     @property
     def var_upper_bounds(self):
-        return 4*np.ones(self.n)
-    
+        return 4 * np.ones(self.n)
+
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
 
 
 class POL(Problem):
@@ -84,6 +101,7 @@ class POL(Problem):
 
     Other name: Poloni’s two objective function
     """
+
     @property
     def n(self):
         return 2
@@ -91,33 +109,39 @@ class POL(Problem):
     @property
     def m(self):
         return 2
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
+
         a1 = 0.5 * np.sin(1) - 2 * np.cos(1) + np.sin(2) - 1.5 * np.cos(2)
         a2 = 1.5 * np.sin(1) - np.cos(1) + 2 * np.sin(2) - 0.5 * np.cos(2)
         b1 = 0.5 * np.sin(x[0]) - 2 * np.cos(x[0]) + np.sin(x[1]) - 1.5 * np.cos(x[1])
         b2 = 1.5 * np.sin(x[0]) - np.cos(x[0]) + 2 * np.sin(x[1]) - 0.5 * np.cos(x[1])
-        return Population(f=np.array([
-            1 + (a1 - b1) ** 2 + (a2 - b2) ** 2,
-            (x[0] + 3) ** 2 + (x[1] + 1) ** 2,
-        ]).T)
-        
+        return Population(
+            f=np.array(
+                [
+                    1 + (a1 - b1) ** 2 + (a2 - b2) ** 2,
+                    (x[0] + 3) ** 2 + (x[1] + 1) ** 2,
+                ]
+            ).T
+        )
+
     @property
     def var_lower_bounds(self):
-        return -np.pi*np.ones(self.n)
-    
+        return -np.pi * np.ones(self.n)
+
     @property
     def var_upper_bounds(self):
-        return np.pi*np.ones(self.n)
+        return np.pi * np.ones(self.n)
 
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
-               
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
+
 
 class KUR(Problem):
     """
@@ -125,6 +149,7 @@ class KUR(Problem):
 
     Other name: Kursawe’s Function
     """
+
     n: int = 3
 
     @property
@@ -134,24 +159,32 @@ class KUR(Problem):
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        return Population(f=np.array([
-            np.sum(-10 * np.exp(-0.2 * np.sqrt(x[:-1] ** 2 + x[1:] ** 2)), axis=0),
-            np.sum(np.abs(x) ** 0.8 + 5 * np.sin(x ** 3), axis=0),
-        ]).T)
+
+        return Population(
+            f=np.array(
+                [
+                    np.sum(
+                        -10 * np.exp(-0.2 * np.sqrt(x[:-1] ** 2 + x[1:] ** 2)), axis=0
+                    ),
+                    np.sum(np.abs(x) ** 0.8 + 5 * np.sin(x**3), axis=0),
+                ]
+            ).T
+        )
 
     @property
     def var_lower_bounds(self):
-        return -5*np.ones(self.n)
-    
+        return -5 * np.ones(self.n)
+
     @property
     def var_upper_bounds(self):
-        return 5*np.ones(self.n)
-    
+        return 5 * np.ones(self.n)
+
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
 
 
 class CONSTR(Problem):
@@ -162,37 +195,33 @@ class CONSTR(Problem):
     @property
     def m(self):
         return 2
-    
+
     @property
     def n_constraints(self):
         return 2
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        f = np.array([
-            x[0],
-            (1 + x[1]) / x[0]
-        ])
-        g = np.array([
-            x[1] + 9 * x[0] - 6,
-            -x[1] + 9 * x[0] - 1
-        ])
+
+        f = np.array([x[0], (1 + x[1]) / x[0]])
+        g = np.array([x[1] + 9 * x[0] - 6, -x[1] + 9 * x[0] - 1])
         return Population(f=f.T, g=g.T)
 
     @property
     def var_lower_bounds(self):
         return np.array([0.1, 0.0])
-    
+
     @property
     def var_upper_bounds(self):
         return np.array([[1.0, 5.0]])
-        
+
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
 
 
 class SRN(Problem):
@@ -203,39 +232,37 @@ class SRN(Problem):
     @property
     def m(self):
         return 2
-    
+
     @property
     def n_constraints(self):
         return 2
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        f = np.array([
-            (x[0] - 2) ** 2 + (x[1] - 1) ** 2 + 2,
-            9 * x[0] - (x[1] - 1) ** 2
-        ])
-        g = np.array([
-            225 - (x[0] ** 2 + x[1] ** 2),
-            -10 - (x[0] - 3 * x[1])
-        ])
+
+        f = np.array(
+            [(x[0] - 2) ** 2 + (x[1] - 1) ** 2 + 2, 9 * x[0] - (x[1] - 1) ** 2]
+        )
+        g = np.array([225 - (x[0] ** 2 + x[1] ** 2), -10 - (x[0] - 3 * x[1])])
         return Population(f=f.T, g=g.T)
-    
+
     @property
     def var_lower_bounds(self):
-        return -20*np.ones(self.n)
-    
+        return -20 * np.ones(self.n)
+
     @property
     def var_upper_bounds(self):
-        return 20*np.ones(self.n)
+        return 20 * np.ones(self.n)
 
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
 
-     
+
 class TNK(Problem):
     @property
     def n(self):
@@ -244,45 +271,47 @@ class TNK(Problem):
     @property
     def m(self):
         return 2
-    
+
     @property
     def n_constraints(self):
         return 2
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        f = np.array([
-            x[0],
-            x[1]
-        ])
-        g = np.array([
-            -(-x[0] ** 2 - x[1] ** 2 + 1 + 0.1 * np.cos(16 * np.arctan(x[0] / x[1]))),
-            0.5 - ((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2)
-        ])
+
+        f = np.array([x[0], x[1]])
+        g = np.array(
+            [
+                -(
+                    -(x[0] ** 2)
+                    - x[1] ** 2
+                    + 1
+                    + 0.1 * np.cos(16 * np.arctan(x[0] / x[1]))
+                ),
+                0.5 - ((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2),
+            ]
+        )
         return Population(f=f.T, g=g.T)
 
     @property
     def var_bounds(self):
-        return np.array([
-            [0.0, 0.0],
-            [np.pi, np.pi]
-        ])
-    
-    
+        return np.array([[0.0, 0.0], [np.pi, np.pi]])
+
     @property
     def var_lower_bounds(self):
         return np.zeros(self.n)
-    
+
     @property
     def var_upper_bounds(self):
-        return np.pi*np.ones(self.n)
+        return np.pi * np.ones(self.n)
 
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
 
 
 class WATER(Problem):
@@ -293,42 +322,48 @@ class WATER(Problem):
     @property
     def m(self):
         return 5
-    
+
     @property
     def n_constraints(self):
         return 7
-    
+
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        f = np.array([
-            106780.37 * (x[1] + x[2]) + 61704.67,
-            3000.0 * x[0],
-            305700 * 2289 * x[1] / (0.06 * 2289) ** 0.65,
-            250 * 2289 * np.exp(-39.75 * x[1] + 9.9 * x[2] + 2.74),
-            25 * (1.39 / (x[0] * x[1]) + 4940 * x[2] - 80)
-        ])
-        g = np.array([
-            1.0 - (0.00139 / (x[0] * x[1]) + 4.94 * x[2] - 0.08),
-            1.0 - (0.000306 / (x[0] * x[1]) + 1.082 * x[2] - 0.0986),
-            50000.0 - (12.307 / (x[0] * x[1]) + 49408.25 * x[2] + 4051.02),
-            16000.0 - (2.098 / (x[0] * x[1]) + 8046.33 * x[2] - 696.71),
-            10000.0 - (2.138 / (x[0] * x[1]) + 7883.39 * x[2] - 705.04),
-            2000.0 - (0.417 / (x[0] * x[1]) + 1721.26 * x[2] - 136.54),
-            550.0 - (0.164 / (x[0] * x[1]) + 631.13 * x[2] - 54.48)
-        ]) 
+
+        f = np.array(
+            [
+                106780.37 * (x[1] + x[2]) + 61704.67,
+                3000.0 * x[0],
+                305700 * 2289 * x[1] / (0.06 * 2289) ** 0.65,
+                250 * 2289 * np.exp(-39.75 * x[1] + 9.9 * x[2] + 2.74),
+                25 * (1.39 / (x[0] * x[1]) + 4940 * x[2] - 80),
+            ]
+        )
+        g = np.array(
+            [
+                1.0 - (0.00139 / (x[0] * x[1]) + 4.94 * x[2] - 0.08),
+                1.0 - (0.000306 / (x[0] * x[1]) + 1.082 * x[2] - 0.0986),
+                50000.0 - (12.307 / (x[0] * x[1]) + 49408.25 * x[2] + 4051.02),
+                16000.0 - (2.098 / (x[0] * x[1]) + 8046.33 * x[2] - 696.71),
+                10000.0 - (2.138 / (x[0] * x[1]) + 7883.39 * x[2] - 705.04),
+                2000.0 - (0.417 / (x[0] * x[1]) + 1721.26 * x[2] - 136.54),
+                550.0 - (0.164 / (x[0] * x[1]) + 631.13 * x[2] - 54.48),
+            ]
+        )
         return Population(f=f.T, g=g.T)
 
     @property
     def var_lower_bounds(self):
         return np.array([0.01, 0.01, 0.01])
-    
+
     @property
     def var_upper_bounds(self):
         return np.array([0.45, 0.10, 0.10])
 
     @property
     def reference(self):
-        return "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "\
-               "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        return (
+            "Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). A fast and elitist multiobjective genetic "
+            "algorithm: NSGA-II. IEEE Transactions on Evolutionary Computation, 6(2), 182–197."
+        )
--- src/paretobench/zdt.py
+++ src/paretobench/zdt.py
@@ -11,16 +11,18 @@ class ZDTx(Problem, ProblemWithPF):
     @property
     def m(self):
         return 2
-    
+
     @property
     def reference(self):
-        return "Zitzler, E., Deb, K., & Thiele, L. (2000). Comparison of Multiobjective Evolutionary Algorithms: Empirical Results. "\
-               "Evolutionary Computation, 8(2), 173–195. https://doi.org/10.1162/106365600568202"
+        return (
+            "Zitzler, E., Deb, K., & Thiele, L. (2000). Comparison of Multiobjective Evolutionary Algorithms: Empirical Results. "
+            "Evolutionary Computation, 8(2), 173–195. https://doi.org/10.1162/106365600568202"
+        )
 
     @property
     def var_lower_bounds(self):
         return np.zeros(self.n)
-    
+
     @property
     def var_upper_bounds(self):
         return np.ones(self.n)
@@ -30,12 +32,16 @@ class ZDT1(ZDTx):
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
+
         g = 1 + 9 * np.sum(x[1:], axis=0) / (self.n - 1)
-        return Population(f=np.array([
-            x[0],
-            g * (1 - np.sqrt(x[0] / g)),
-        ]).T)
+        return Population(
+            f=np.array(
+                [
+                    x[0],
+                    g * (1 - np.sqrt(x[0] / g)),
+                ]
+            ).T
+        )
 
     def get_pareto_front(self, n):
         x = np.linspace(0, 1, n)
@@ -46,28 +52,36 @@ class ZDT2(ZDTx):
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
+
         g = 1 + 9 * np.sum(x[1:], axis=0) / (self.n - 1)
-        return Population(f=np.array([
-            x[0],
-            g * (1 - (x[0] / g) ** 2),
-        ]).T)
+        return Population(
+            f=np.array(
+                [
+                    x[0],
+                    g * (1 - (x[0] / g) ** 2),
+                ]
+            ).T
+        )
 
     def get_pareto_front(self, n):
         x = np.linspace(0, 1, n)
         return np.array([x, 1 - x**2]).T
-    
+
 
 class ZDT3(ZDTx):
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
+
         g = 1 + 9 * np.sum(x[1:], axis=0) / (self.n - 1)
-        return Population(f=np.array([
-            x[0],
-            g * (1 - np.sqrt(x[0] / g) - x[0] / g * np.sin(10 * np.pi * x[0])),
-        ]).T)
+        return Population(
+            f=np.array(
+                [
+                    x[0],
+                    g * (1 - np.sqrt(x[0] / g) - x[0] / g * np.sin(10 * np.pi * x[0])),
+                ]
+            ).T
+        )
 
     def get_pareto_front(self, n):
         # The non-dominated regions from dev_notebooks\zdt3_pareto_front.ipynb
@@ -76,14 +90,14 @@ class ZDT3(ZDTx):
             (0.18222872351573624, 0.25776236490626825),
             (0.4093136698086569, 0.453882099086383),
             (0.6183967894392438, 0.6525116988130034),
-            (0.8233317933269128, 0.851832862382744)
+            (0.8233317933269128, 0.851832862382744),
         ]
-        
+
         # Evaluate along the non-dominated region
         f = []
         for r, my_n in zip(regs, weighted_chunk_sizes(n, [r[1] - r[0] for r in regs])):
             x = np.linspace(*r, my_n)
-            f.append([x, 1 - np.sqrt(x) - x*np.sin(10*np.pi*x)])
+            f.append([x, 1 - np.sqrt(x) - x * np.sin(10 * np.pi * x)])
         return np.concatenate(f, axis=1).T
 
 
@@ -93,20 +107,28 @@ class ZDT4(ZDTx):
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        g = 1 + 10 * (self.n - 1) + np.sum(x[1:] ** 2 - 10 * np.cos(4 * np.pi * x[1:]), axis=0)
-        return Population(f=np.array([
-            x[0],
-            g * (1 - np.sqrt(x[0] / g)),
-        ]).T)
+
+        g = (
+            1
+            + 10 * (self.n - 1)
+            + np.sum(x[1:] ** 2 - 10 * np.cos(4 * np.pi * x[1:]), axis=0)
+        )
+        return Population(
+            f=np.array(
+                [
+                    x[0],
+                    g * (1 - np.sqrt(x[0] / g)),
+                ]
+            ).T
+        )
 
     @property
     def var_lower_bounds(self):
-        return np.concatenate(([0], -5*np.ones(self.n-1)))
-    
+        return np.concatenate(([0], -5 * np.ones(self.n - 1)))
+
     @property
     def var_upper_bounds(self):
-        return np.concatenate(([1], 5*np.ones(self.n-1)))
+        return np.concatenate(([1], 5 * np.ones(self.n - 1)))
 
     def get_pareto_front(self, n):
         x = np.linspace(0, 1, n)
@@ -119,13 +141,17 @@ class ZDT6(ZDTx):
     def _call(self, x):
         # Transpose x (this function was written before ParetoBench standardized on rows being the batched index)
         x = x.T
-        
-        f1 = 1 - np.exp(-4*x[0])*(np.sin(6*np.pi*x[0]))**6
-        g = 1 + 9 * (np.sum(x[1:], axis=0) / (self.n - 1))**0.25
-        return Population(f=np.array([
-            f1,
-            g * (1 - (f1 / g) ** 2),
-        ]).T)
+
+        f1 = 1 - np.exp(-4 * x[0]) * (np.sin(6 * np.pi * x[0])) ** 6
+        g = 1 + 9 * (np.sum(x[1:], axis=0) / (self.n - 1)) ** 0.25
+        return Population(
+            f=np.array(
+                [
+                    f1,
+                    g * (1 - (f1 / g) ** 2),
+                ]
+            ).T
+        )
 
     def get_pareto_front(self, n):
         # From dev_notebooks\zdt6_pareto_front.ipynb
