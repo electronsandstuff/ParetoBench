33b0e8cff0952de8105d7fc66ada1db622f0b8f4
Christopher M. Pierce <chris@chris-pierce.com>
2024-12-09 22:35:37 -0800

    automatic formatting on all files

--- src/paretobench/utils.py
+++ src/paretobench/utils.py
@@ -4,38 +4,43 @@ from math import comb
 
 
 def get_betas(m, p):
-    '''
+    """
     From: Das, Indraneel, and J. E. Dennis. “Normal-Boundary Intersection: A New Method for
-    Generating the Pareto Surface in Nonlinear Multicriteria Optimization Problems.” SIAM 
+    Generating the Pareto Surface in Nonlinear Multicriteria Optimization Problems.” SIAM
     Journal on Optimization 8, no. 3 (August 1998): 631–57. https://doi.org/10.1137/S1052623496307510.
-    '''
-    beta = np.fromiter(chain.from_iterable(combinations(range(1, p+m), m-1)), np.float64)
-    beta = beta.reshape(beta.shape[0]//(m-1), m-1).T
-    beta = beta - np.arange(0, m-1)[:, None] - 1
+    """
+    beta = np.fromiter(
+        chain.from_iterable(combinations(range(1, p + m), m - 1)), np.float64
+    )
+    beta = beta.reshape(beta.shape[0] // (m - 1), m - 1).T
+    beta = beta - np.arange(0, m - 1)[:, None] - 1
     beta1 = np.concatenate((beta, np.full((1, beta.shape[1]), p)), axis=0)
     beta2 = np.concatenate((np.zeros((1, beta.shape[1])), beta), axis=0)
-    return (beta1 - beta2)/p
+    return (beta1 - beta2) / p
 
 
 def get_hyperplane_points(m, n):
-    '''
+    """
     Returns at least n points of dimension m on the hyperplane x1 + x2 + x3 + ... = 1
-    
+
     From: Das, Indraneel, and J. E. Dennis. “Normal-Boundary Intersection: A New Method for
-    Generating the Pareto Surface in Nonlinear Multicriteria Optimization Problems.” SIAM 
+    Generating the Pareto Surface in Nonlinear Multicriteria Optimization Problems.” SIAM
     Journal on Optimization 8, no. 3 (August 1998): 631–57. https://doi.org/10.1137/S1052623496307510.
-    '''
-    return get_betas(m, next(p for p in count() if comb(p+m-1, m-1) >= n))
+    """
+    return get_betas(m, next(p for p in count() if comb(p + m - 1, m - 1) >= n))
 
 
 def uniform_grid(n, m):
-    '''
+    """
     At lesat n evenly spread points in the hypercube of dimension m
-    '''
-    return np.reshape(np.stack(np.meshgrid(*([np.linspace(0, 1, int(np.ceil(n**(1/m))))]*m))), (m, -1))
+    """
+    return np.reshape(
+        np.stack(np.meshgrid(*([np.linspace(0, 1, int(np.ceil(n ** (1 / m))))] * m))),
+        (m, -1),
+    )
 
 
-def fast_dominated_argsort(O):
+def fast_dominated_argsort(objs):
     """
     Performs a dominated sort on matrix of objective function values O.  This is a numpy implementation of the algorithm
     described in Deb, K., Pratap, A., Agarwal, S., & Meyarivan, T. (2002). IEEE Transactions on Evolutionary
@@ -47,7 +52,10 @@ def fast_dominated_argsort(O):
     :return: List of ranks where each rank is a list of the indices to the individuals in that rank
     """
     # Compare all pairs of individuals based on domination
-    dom = np.bitwise_and((O[:, :, None] <= O[:, None, :]).all(axis=0), (O[:, :, None] < O[:, None, :]).any(axis=0))
+    dom = np.bitwise_and(
+        (objs[:, :, None] <= objs[:, None, :]).all(axis=0),
+        (objs[:, :, None] < objs[:, None, :]).any(axis=0),
+    )
 
     # Create the sets of dominated individuals, domination number, and first rank
     S = [np.nonzero(row)[0].tolist() for row in dom]
@@ -71,22 +79,27 @@ def fast_dominated_argsort(O):
     return F
 
 
-def get_nondominated(O):
+def get_nondominated(objs):
     """
     Returns the indices of the nondominated individuals for the objectives O, an (m,n) array for the m objectives
     """
-    dom = np.bitwise_and((O[:, :, None] <= O[:, None, :]).all(axis=0), (O[:, :, None] < O[:, None, :]).any(axis=0))
+    dom = np.bitwise_and(
+        (objs[:, :, None] <= objs[:, None, :]).all(axis=0),
+        (objs[:, :, None] < objs[:, None, :]).any(axis=0),
+    )
     return np.where(np.sum(dom, axis=0) == 0)[0].tolist()
 
 
 def triangle_grid_count(n):
-    return n*(n+1)//2
+    return n * (n + 1) // 2
 
 
 def triangle_grid(n):
-    # skewed points to help compensate for 
-    x = np.concatenate([np.linspace(0, 1 - np.sqrt(i/(n-1)), n-i) for i in range(n)])
-    y = np.concatenate([np.ones(n-i)*np.sqrt(i/(n-1)) for i in range(n)])
+    # skewed points to help compensate for
+    x = np.concatenate(
+        [np.linspace(0, 1 - np.sqrt(i / (n - 1)), n - i) for i in range(n)]
+    )
+    y = np.concatenate([np.ones(n - i) * np.sqrt(i / (n - 1)) for i in range(n)])
     return np.vstack((x, y))
 
 
@@ -102,7 +115,7 @@ def rastrigin(x):
     :return: g(x), the Rastrigin function
     """
     a = 10
-    return 1 + a*x.shape[0] + np.sum(x**2 - a*np.cos(2*np.pi*x), axis=0)
+    return 1 + a * x.shape[0] + np.sum(x**2 - a * np.cos(2 * np.pi * x), axis=0)
 
 
 def weighted_chunk_sizes(n, weights):
@@ -117,10 +130,10 @@ def weighted_chunk_sizes(n, weights):
     Returns:
         list: The size of each chunk summing to n
     """
-    ns = [int(np.floor(n*w/sum(weights))) for w in weights]
+    ns = [int(np.floor(n * w / sum(weights))) for w in weights]
     for i in range(32):
         if sum(ns) < n:
-            ns[i%len(ns)] += 1
+            ns[i % len(ns)] += 1
         else:
             break
     return ns
