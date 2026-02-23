1d68d44f82a308d3595ad29fdce37e05743ddc2a
Christopher M. Pierce <chris@chris-pierce.com>
2026-02-22 19:43:12 -0800

    test for `import_cnsga_history`

--- tests/ext/test_xopt.py
+++ tests/ext/test_xopt.py
@@ -3,11 +3,12 @@ import tempfile
 import pandas as pd
 from xopt.base import Xopt
 from xopt.evaluator import Evaluator
+from xopt.generators.ga.cnsga import CNSGAGenerator
 from xopt.generators.ga.nsga2 import NSGA2Generator
 from xopt.resources.test_functions.tnk import evaluate_TNK, tnk_vocs
 import numpy as np
 
-from paretobench.ext.xopt import import_nsga2_history
+from paretobench.ext.xopt import import_cnsga_history, import_nsga2_history
 
 
 def test_import_nsga2_history():
@@ -87,3 +88,68 @@ def test_import_nsga2_history():
         # Close log file before exiting context
         finally:
             xx.generator.close_log_file()
+
+
+def test_import_cnsga_history():
+    population_size = 32
+    n_generations = 3
+    with tempfile.TemporaryDirectory() as output_dir:
+        generator = CNSGAGenerator(
+            vocs=tnk_vocs,
+            output_path=output_dir,
+            population_size=population_size,
+        )
+
+        xx = Xopt(
+            generator=generator,
+            evaluator=Evaluator(function=evaluate_TNK, max_workers=1),
+            vocs=tnk_vocs,
+        )
+
+        # Save xopt config to yaml
+        xx.dump(os.path.join(output_dir, "xopt.yml"))
+
+        # Run generations and capture population data to test against
+        xopt_xs = []
+        xopt_fs = []
+        xopt_gs = []
+        for _ in range(n_generations):
+            for _ in range(population_size):
+                xx.step()
+            xopt_xs.append(xx.generator.vocs.variable_data(xx.generator.population))
+            xopt_fs.append(xx.generator.vocs.objective_data(xx.generator.population))
+            xopt_gs.append(xx.generator.vocs.constraint_data(xx.generator.population))
+
+        # Save vocs to json for file-based loading test
+        vocs_path = os.path.join(output_dir, "vocs.json")
+        with open(vocs_path, "w") as f:
+            f.write(xx.generator.vocs.model_dump_json())
+
+        cases = [
+            (vocs_path, None),
+            (xx.generator.vocs, None),
+            (None, os.path.join(output_dir, "xopt.yml")),
+        ]
+        for vocs, config in cases:
+            test_hist = import_cnsga_history(output_dir, vocs=vocs, config=config)
+
+            assert len(xopt_xs) == len(test_hist)
+            for rx, rf, rg, tp in zip(xopt_xs, xopt_fs, xopt_gs, test_hist.reports):
+                assert len(rx) == len(tp)
+                assert rf.shape[1] == tp.m
+                assert rx.shape[1] == tp.n
+                assert rg.shape[1] == tp.n_constraints
+
+                def lexsorted(arr):
+                    return arr[np.lexsort(arr.T[::-1])]
+
+                def df_comp(df1, df2):
+                    assert set(df1.columns) == set(df2.columns)
+                    cols = sorted(df1.columns)
+                    np.testing.assert_allclose(lexsorted(df1[cols].to_numpy()), lexsorted(df2[cols].to_numpy()))
+
+                df_comp(rx, pd.DataFrame(tp.x, columns=tp.names_x))
+                rf.columns = [x.removeprefix("objective_") for x in rf.columns]
+                df_comp(rf, pd.DataFrame(tp.f, columns=tp.names_f))
+                rg.columns = [x.removeprefix("constraint_") for x in rg.columns]
+                df_comp(rg, pd.DataFrame(tp.g_canonical, columns=tp.names_g))
